8285244a804070a22320b1b5829ed76008e0cccb
caualcruz <caualucas34492@gmail.com>
2025-10-22 19:27:00 -0300

    first commit

---
+++ src/main/java/agenda_de_contatos/cc_agenda_de_contatos/HelloController.java
@@ -0,0 +1,14 @@
+package agenda_de_contatos.cc_agenda_de_contatos;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
---
+++ src/main/java/agenda_de_contatos/cc_agenda_de_contatos/Launcher.java
@@ -0,0 +1,9 @@
+package agenda_de_contatos.cc_agenda_de_contatos;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
