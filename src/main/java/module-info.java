bec222cc94608c3de93aeaf195f17502bbf508fb
caualcruz <caualucas34492@gmail.com>
2025-10-23 19:26:59 -0300

    Projeto rodando puro

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,12 +1,16 @@
-module agenda_de_contatos.cc_agenda_de_contatos {
+module agenda_de_contatos {
     requires javafx.controls;
     requires javafx.fxml;
+    requires java.sql;
 
+    requires java.dotenv;
     requires org.controlsfx.controls;
     requires com.dlsc.formsfx;
     requires net.synedra.validatorfx;
     requires org.kordamp.bootstrapfx.core;
 
-    opens agenda_de_contatos.cc_agenda_de_contatos to javafx.fxml;
-    exports agenda_de_contatos.cc_agenda_de_contatos;
+    opens agenda_de_contatos to javafx.fxml;
+    opens agenda_de_contatos.controller to javafx.fxml;
+    opens agenda_de_contatos.model to javafx.base;
+    exports agenda_de_contatos;
 }
