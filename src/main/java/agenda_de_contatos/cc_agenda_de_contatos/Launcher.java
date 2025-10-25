73072ada071e59611b4b0210c6f3844b5907f013
caualcruz <caualucas34492@gmail.com>
2025-10-24 22:00:05 -0300

    Começando novos temas

--- src/main/java/agenda_de_contatos/cc_agenda_de_contatos/Launcher.java
+++
@@ -1,9 +0,0 @@
-package agenda_de_contatos.cc_agenda_de_contatos;
-
-import javafx.application.Application;
-
-public class Launcher {
-    public static void main(String[] args) {
-        Application.launch(HelloApplication.class, args);
-    }
-}
